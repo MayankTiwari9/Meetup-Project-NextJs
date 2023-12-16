d52e97d774f07a4b1fc403c544b728e415a7cd9e
MayankTiwari9 <mt1027319@gmail.com>
2023-12-16 12:12:14 +0530

    Updated

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,10 +1,11 @@
 import { useRouter } from "next/router";
 import Card from "../../components/ui/Card";
 import classes from "../../components/meetups/MeetupItem.module.css";
+import Image from "next/image";
 
 const MeetupDetail = () => {
-  const router = useRouter().query;
-  const id = router.meetupId;
+  const router = useRouter();
+  const { meetupId } = router.query;
 
   const DUMMY_MEETUPS = [
     {
@@ -23,23 +24,82 @@ const MeetupDetail = () => {
       address: "Some address 10, 12345 Some City",
       description: "This is a second meetup!",
     },
+    {
+      id: "m3",
+      title: "A third Meetup",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+      address: "Some address 10, 12345 Some City",
+      description: "This is a Third meetup!",
+    },
   ];
 
-  const meetup = DUMMY_MEETUPS.find((meet) => meet.id === id);
+  const meetup = DUMMY_MEETUPS.find((meet) => meet.id === meetupId);
 
   return (
     <>
-      <Card>
-        <div className={classes.image}>
-          <img src={meetup.image} alt={meetup.title} />
-        </div>
-        <div className={classes.content}>
-          <h3>{meetup.title}</h3>
-          <address>{meetup.address}</address>
-        </div>
-      </Card>
+      {meetup && (
+        <Card>
+          <div className={classes.image}>
+            <Image
+              src={meetup.image}
+              alt={meetup.title}
+              width={1280}
+              height={720}
+            />
+          </div>
+          <div className={classes.content}>
+            <h3>{meetup.title}</h3>
+            <address>{meetup.address}</address>
+          </div>
+        </Card>
+      )}
     </>
   );
 };
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetups
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
 
 export default MeetupDetail;
